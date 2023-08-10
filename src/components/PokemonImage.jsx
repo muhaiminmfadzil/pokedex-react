42a82078312252ddc37ada1d655e2a5d3f9bfaaf
muhaimin <meenkun4649@gmail.com>
2023-08-10 23:21:46 +0800

    update: fetch api

--- src/components/PokemonImage.jsx
+++ src/components/PokemonImage.jsx
@@ -1,10 +1,17 @@
-function PokemonImage() {
+import PropTypes from 'prop-types'
+
+function PokemonImage({ number }) {
   return (
     <>
       <div className='flex items-center justify-center mx-auto'>
-        <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png' alt='' />
+        <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${number}.png`} alt='' />
       </div>
     </>
   )
 }
+
+PokemonImage.propTypes = {
+  number: PropTypes.string.isRequired,
+}
+
 export default PokemonImage
--- src/pages/PokemonsView.jsx
+++ src/pages/PokemonsView.jsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import PokemonCard from '../components/PokemonCard'
 
 function PokemonsView() {
-  const listing = new Array(20).fill().map((x, idx) => idx)
+  const [pokemons, setPokemons] = useState([])
+  const [isError, setIsError] = useState(false)
+
+  const sanitizeData = (obj) => {
+    let newObj = { ...obj }
+    // Number
+    const splitUrl = obj.url.split('/')
+    newObj.number = String(splitUrl[splitUrl.length - 2]).padStart(3, '0')
+
+    return newObj
+  }
+
+  useEffect(() => {
+    const params = {
+      limit: 20,
+      offset: 0,
+    }
+    const fetchPokemons = async () => {
+      try {
+        const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon', { params })
+        const pokemons = data.results.map((result) => sanitizeData(result))
+        setPokemons(pokemons)
+      } catch (error) {
+        setIsError(true)
+      }
+    }
+
+    fetchPokemons()
+  }, [])
+
+  if (isError) {
+    return <h3>Is Error...</h3>
+  }
 
   return (
     <>
@@ -9,8 +43,8 @@ function PokemonsView() {
       <img className='object-cover w-64 h-32 pt-4 mx-auto' src='https://www.freepnglogos.com/uploads/pokemon-logo-png-0.png' alt='Pokemon Logo Png' />
       {/* Listing */}
       <ul className='grid max-w-4xl grid-cols-1 gap-6 px-8 pt-8 pb-[80px] mx-auto bg-blue-200 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-        {listing.map(() => (
-          <PokemonCard />
+        {pokemons.map((pokemon) => (
+          <PokemonCard key={pokemon.number} number={pokemon.number} name={pokemon.name} />
         ))}
       </ul>
     </>
